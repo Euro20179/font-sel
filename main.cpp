bb679302685bfa7b17b8f0aa6722cbd17ae0a659
Euro20179 <sandollar2020@gmail.com>
2022-06-13 13:19:23 -0700

    add scrolling

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <gtk/gtk.h>
 #include <iostream>
 
+#define WINDOW_WIDTH 500
+#define WINDOW_HEIGHT 500
+
 static GtkWidget* label;
 
 static void fontSet(GtkFontChooser* fontButton, gpointer data){
@@ -18,25 +21,32 @@ activate (GtkApplication* app,
   GtkWidget* window;
 
   GtkWidget* fontButton;
-  GtkWidget* hbox;
+  GtkWidget* vbox;
+  GtkWidget* scrolledWindow;
 
   const char* css =  "label#font-label{font-family: Comic Sans MS}";
 
   window = gtk_application_window_new (app);
   gtk_window_set_title (GTK_WINDOW (window), "Font Sel");
   gtk_widget_set_name(window, "Font-Sel-Window");
-  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+  gtk_window_set_default_size (GTK_WINDOW (window), WINDOW_WIDTH, WINDOW_HEIGHT);
 
 
-  hbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+  vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+  gtk_box_set_homogeneous(GTK_BOX(vbox), false);
+
   fontButton = gtk_font_button_new();
-  gtk_container_add(GTK_CONTAINER(hbox), fontButton);
+  gtk_container_add(GTK_CONTAINER(vbox), fontButton);
+
+  scrolledWindow = gtk_scrolled_window_new(NULL, NULL);
+  gtk_scrolled_window_set_min_content_height(GTK_SCROLLED_WINDOW(scrolledWindow), WINDOW_HEIGHT);
+  gtk_container_add(GTK_CONTAINER(vbox), scrolledWindow);
 
   label = gtk_label_new("the quick brown fox jumped over the lazy dog\nTHE QUICK BROWN FOX JUMPED OVER THE LAZY DOG\n😀❤\n:) :( :-) :-(\n1234567890\n!@#$%^&*()-=_+/\\,.;':\"\n[] {} ()");
   gtk_widget_set_name(label, "font-label");
-  gtk_container_add(GTK_CONTAINER(hbox), label);
+  gtk_container_add(GTK_CONTAINER(scrolledWindow), label);
 
-  gtk_container_add(GTK_CONTAINER(window), hbox);
+  gtk_container_add(GTK_CONTAINER(window), vbox);
 
   PangoFontDescription* fontd = pango_font_description_from_string("Comic Sans MS");
 
@@ -47,17 +57,14 @@ activate (GtkApplication* app,
   gtk_widget_show_all (window);
 }
 
-int
-main (int    argc,
-      char **argv)
-{
-  GtkApplication *app;
-  int status;
+int main (int argc, char **argv){
+    GtkApplication *app;
+    int status;
 
-  app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-  status = g_application_run (G_APPLICATION (app), argc, argv);
-  g_object_unref (app);
+    app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+    g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+    status = g_application_run (G_APPLICATION (app), argc, argv);
+    g_object_unref (app);
 
-  return status;
+    return status;
 }
